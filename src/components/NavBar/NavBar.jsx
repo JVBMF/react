1af93ed6f32334464ac7b2ef684dad4baff460d2
JVBMF <juann.decibe@gmail.com>
2022-02-22 17:23:33 -0300

    first commit

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './NavBar.css';
+
+const NavBar = () => {
+ return(
+ <ul>
+     <li>Logo</li>
+     <li>Zapatillas</li>
+     <li>Remeras</li>
+     <li>Pantalones</li>
+ </ul>
+ )
+}
+
+export { NavBar }
