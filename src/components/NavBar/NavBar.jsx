fb66185a33f5516b2a413f52bcc610ec54d9d05c
JVBMF <juann.decibe@gmail.com>
2022-02-22 18:01:22 -0300

    Entrega 2

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,8 +4,8 @@ import './NavBar.css';
 const NavBar = () => {
  return(
     <nav class="menu">
-        <ul class="menuItem">
-            <li><a href="#">Logo</a></li>
+        <ul class="menuItems">
+            <li><a href="#">Logo</a></li> {/* después agrego las páginas a las que te redirige */}
             <li><a href="#">Zapatillas</a></li>
             <li><a href="#">Remeras</a></li>
             <li><a href="#">Pantalones</a></li>
